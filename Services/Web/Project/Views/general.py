9b12d35acb0ca6dab1391900ebf0f455c9fd21a7
ThomasBabato <65108595+ThomasBabato@users.noreply.github.com>
2021-11-23 13:07:32 +0100

    Inizio blueprint
    Inizio di sviluppo con blueprint. Nulla di serio. Proseguo oggi pomeriggio quando riavvio su windows.

---
+++ Services/Web/Project/Views/general.py
@@ -0,0 +1,9 @@
+#primo file per la prima blueprint. Questa blueprint sarà per le pagine senza login e per la pagina di login e registrazione
+
+
+#mod = Blueprint('general',__name__)
+
+# homepage
+#@mod.route('/')
+#def index():
+    #render template
