049b729bc94060894ed53cb9ee643f1308c693d1
Haka91 <31970308+Haka91@users.noreply.github.com>
2021-12-16 16:32:41 +0100

    rework courses to comply with standard

--- Services/Web/Project/Models/Lessons.py
+++ Services/Web/Project/Models/Lessons.py
@@ -26,21 +26,21 @@ class Lesson(Base):
 
     id = Column("id",Integer, primary_key=True)
     reservation_slot=Column(Integer,ForeignKey("Reservation_slots.id"),nullable=False) 
-    course = Column(Integer,ForeignKey("Courses.id"),nullable=False)
-    trainer = Column(Integer,ForeignKey("Users.id"),nullable=False)
+    course = Column(Integer,ForeignKey("Courses.id"),nullable=False)   
     course_room = Column(Integer,ForeignKey("Course_Rooms.id"),nullable=False)
+
 
     course_room_obj=relationship("Course_Room",back_populates="lessons_obj")
     course_obj =  relationship("Course",back_populates="lessons_obj")
-    trainer_obj= relationship("User", back_populates="lessons_obj")
+   
     reservation_slot_obj=relationship("Reservation_Slot",back_populates="lessons_obj")
     course_reservations_obj=relationship("Course_Reservation",back_populates="lesson_obj")
     
     
-    def __init__(self,reservation_slot,course,trainer,course_room):
+    def __init__(self,reservation_slot,course,course_room):
         self.reservation_slot=reservation_slot
         self.course=course
-        self.trainer=trainer
+       
         self.course_room=course_room
 
 
